f8e15016be91d517778a65c6c35eebe8080238fb
Rakesh Sangare <rakeshsangare93@gmail.com>
2023-08-18 11:52:27 +0530

    second commit

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# creating home page
+# creating home function
 def home(request):
     return render(request,'pages/home.html')
+
+# creating about function
+def about(request):
+    return render(request,'pages/about.html')
+
+#creating services function
+def services(request):
+    return render(request,'pages/services.html')
+
+#creating contact function
+def contact(request):
+    return render(request,'pages/contact.html')
+
