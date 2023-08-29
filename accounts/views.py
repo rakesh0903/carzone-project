953463b5d518d296e7ea0543ae55d8d47ba722ed
Rakesh Sangare <rakeshsangare93@gmail.com>
2023-08-29 16:12:31 +0530

    registration functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
-
+from django.contrib import messages,auth
+from django.contrib.auth.models import User
 # Create your views here.
 
 #login function base view
@@ -10,7 +11,37 @@ def logout(request):
     return redirect('pages:home')
 
 def register(request):
-    return render(request,'accounts/register.html')
+    if request.method == "POST":
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request,"Username Already exist")
+                return redirect('accounts:register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request,"Email already exist")
+                    return redirect('accounts:register')
+                else:
+                    user = User.objects.create_user(first_name=firstname,last_name=lastname,email=email,username=username,password=password)
+                    auth.login(request,user)
+                    messages.success(request,"you are now log in")
+                    return redirect('accounts:dashboard')
+                    user.save()
+                    messages.success(request,"You are register successfully.")
+                    return redirect('accounts:login')
+        else:
+            messages.error(request,"password do not matching")
+            return redirect('accounts:register')
+
+        
+    else:
+        return render(request,'accounts/register.html')
 
 def dashboard(request):
     return render(request,'accounts/dashboard.html')
--- carzone/settings.py
+++ carzone/settings.py
@@ -137,3 +137,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media settings
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+
+#Message tags
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+    50: "critical",
+}
